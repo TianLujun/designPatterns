e815801c9ef78bc6e03aab5af7487474c4de008f
TianLujun <tianlujunr@gmail.com>
2018-12-04 11:24:39 +0800

    Update Product.java

--- src/factory/Product.java
+++ src/factory/Product.java
@@ -2,7 +2,7 @@ package factory;
 
 public abstract class Product {
     public void method1() {
-        
+        System.out.println("This is a product.");
     }
     
     public abstract void method2();
