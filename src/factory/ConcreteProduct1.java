5c604e0ed963179cb45f98deee0290afb450c137
TianLujun <tianlujunr@gmail.com>
2018-12-04 11:22:18 +0800

    Update ConcreteProduct1.java

--- src/factory/ConcreteProduct1.java
+++ src/factory/ConcreteProduct1.java
@@ -3,7 +3,6 @@ package factory;
 public class ConcreteProduct1 extends Product {
 
     public void method2() {
-        // TODO Auto-generated method stub
         System.out.println("This is a product 1.");
     }
 
